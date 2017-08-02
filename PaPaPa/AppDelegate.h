8790e7ea2a5c895b7a77e0bb8dedc06cd717df31
cht <cht_net@126.com>
2017-08-02 11:05:04 +0800

    Initial Commit

---
+++ PaPaPa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PaPaPa
+//
+//  Created by CHT on 2017/8/2.
+//  Copyright © 2017年 chihaitao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PaPaPa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PaPaPa
+//
+//  Created by CHT on 2017/8/2.
+//  Copyright © 2017年 chihaitao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
